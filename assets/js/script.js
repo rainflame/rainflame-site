e0734f4b67daaa663c7564639b9e55f6de7037fa
Christian Broms <cb@christianbroms.com>
2020-05-28 12:58:38 -0700

    remove shaders from mobile and add still image, add some text to front page

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,27 +2,34 @@ const canvas = document.getElementById("backgroundCanvas");
 // canvas size
 let w, h;
 let loop = null;
+let logo = null;
 
 const setSize = () => {
-  w = window.innerWidth > 800 ? 800 : window.innerWidth;
-  h = window.innerHeight > 1000 ? 1000 : window.innerHeight;
+  w = window.innerWidth / 2 > 800 ? 800 : window.innerWidth / 2;
+  h = window.innerHeight > 1100 ? 1100 : window.innerHeight;
   canvas.width = w;
   canvas.height = h;
   if (loop !== null) {
-    loop.glCanvas.style.left = `${window.innerWidth / 2 - w / 2}px`;
+    loop.glCanvas.style.left = `${window.innerWidth / 3 - w / 2}px`;
     loop.glCanvas.style.top = `${window.innerHeight / 2 - h / 2}px`;
-    canvas.style.display = "none";
+    if (w < 600) {
+      loop.stopLoop();
+      loadImage();
+    } else {
+      loop.startLoop();
+      canvas.style.display = "none";
+    }
   }
+};
+
+const loadImage = () => {
+  logo = new Image();
+  logo.src = w > 600 ? "/assets/logo.png" : "/assets/background.png";
 };
 
 window.addEventListener("resize", setSize);
 setSize();
-
-// load the image
-// Create the image object
-const logo = new Image();
-// Set the source url of the image
-logo.src = "/assets/logo.png";
+loadImage();
 
 // textbox size
 const bw = 330;
@@ -39,22 +46,25 @@ setSize();
 const start = async () => {
   // load the font
   const fg = new V4.FontGroup();
-  await fg.loadFont("/assets/fonts/ant.ttf", "Black");
+  await fg.loadFont("/assets/fonts/AntiquaBold.ttf", "Black");
 
   // background logo renderer
   const logoRenderer = (state) => {
     let x = 0;
     let y = 0;
-    for (let c = 0; c < w / 100; c++) {
-      y = c % 2 == 0 ? 0 : -50;
-      for (let r = 0; r < (h + 100) / 100; r++) {
-        state.context.drawImage(logo, x, y, 100, 100);
-        y += 100;
+    if (logo.src.includes("/assets/logo.png")) {
+      for (let c = 0; c < w / 100; c++) {
+        y = c % 2 == 0 ? 0 : -50;
+        for (let r = 0; r < (h + 100) / 100; r++) {
+          state.context.drawImage(logo, x, y, 100, 100);
+          y += 100;
+        }
+        x += 100;
       }
-      x += 100;
+    } else {
+      state.context.drawImage(logo, x, y);
     }
   };
-
   loop.addToLoop(logoRenderer);
 
   // bouncing text renderer
@@ -71,37 +81,41 @@ const start = async () => {
   tb.text("Rainflame");
 
   // load and set the shader
-  const sr = new V4.Shader(loop.glCanvas);
-  const shader = await sr.loadShader("/assets/vhsFilter.glsl");
-  sr.useCanvasState(true);
-  sr.setShader(shader);
-  loop.addToLoop(sr);
+  if (w > 600) {
+    const sr = new V4.Shader(loop.glCanvas);
+    const shader = await sr.loadShader("/assets/vhsFilter.glsl");
+    sr.useCanvasState(true);
+    sr.setShader(shader);
+    loop.addToLoop(sr);
+  }
 
-  // // set the textbox and canvas colors to be a random blue shade
-  // const randomBlue = () => {
-  //   const blue = Math.floor(Math.random() * 180) + 75;
-  //   tb.options({ backgroundColor: `rgb(0, 0, ${blue})` });
-  //   loop.backgroundColor(`rgb(0, 0, ${255 - blue})`);
-  // };
+  // set the textbox and canvas colors to be a random blue shade
+  const randomBlue = () => {
+    const blue = Math.floor(Math.random() * 100) + 75;
+    tb.options({ backgroundColor: `rgb(50, 75, ${blue})` });
+    loop.opts.backgroundColor = `rgb(50, 75, ${255 - blue})`;
+  };
 
   // the renderer to bounce the textbox around the canvas
   const bounceRenderer = (state) => {
     if (x + bw >= w || x <= 0) {
       xMag *= -1;
       x += 5 * Math.sign(xMag); // give it a little boost to prevent sticking to side
+      randomBlue();
     } else if (y >= h || y <= bh) {
       yMag *= -1;
       y += 5 * Math.sign(yMag);
+      randomBlue();
     }
     x += xMag * state.deltaTime;
     y += yMag * state.deltaTime;
 
     tb.options({ position: { x, y } });
-    tb.renderer(state);
+    if (w > 600) tb.renderer(state);
   };
 
   // add the renderer to the loop and start it
-  loop.addToLoop(bounceRenderer);
+  if (w > 600) loop.addToLoop(bounceRenderer);
   loop.framesPerSecond(24);
   loop.startLoop();
 };
